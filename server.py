4911ad4cd1955e2797c3d4e563be95ba13afff1a
Sumit Das <sumitdas0849@gmail.com>
2024-08-17 21:01:59 +0530

    using "requests" library instead of "httpx"

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-import httpx
+import requests, traceback
 import uvicorn
 from authorization.auth import get_spotify_token
 
@@ -8,17 +8,18 @@ app = FastAPI()
 
 # Route to get an artist's information
 @app.get("/artist/{artist_id}", response_class=JSONResponse)
-async def get_artist(artist_id: str):
-    token = await get_spotify_token()
-    artist_response = await httpx.get(
-        f"https://api.spotify.com/v1/artists/{artist_id}",
-        headers={
-            "Authorization": f"Bearer {token}"
-        }
+def get_artist(artist_id: str):
+    token = get_spotify_token()
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    artist_response = requests.get(
+        f"https://api.spotify.com/v1/artists/{artist_id}", headers = headers
     )
     if artist_response.status_code == 200:
         return artist_response.json()
     else:
+        traceback.print_exc()
         raise HTTPException(status_code=artist_response.status_code, detail="Failed to fetch artist information")
 
 if __name__ == "__main__":
