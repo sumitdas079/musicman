db5d5eec0301a286760c45f75a637caf12ec74b1
Sumit Das <sumitdas0849@gmail.com>
2024-08-17 21:01:25 +0530

    refactored token retrieval code

--- authorization/auth.py
+++ authorization/auth.py
@@ -1,5 +1,6 @@
 import os
-import httpx
+import requests
+import httpx, base64, traceback
 from fastapi import HTTPException
 
 # Load environment variables
@@ -7,26 +8,46 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Spotify API credentials
-SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 # Spotify API token
-SPOTIFY_TOKEN = None
+# SPOTIFY_TOKEN = None
 
-async def get_spotify_token():
-    global SPOTIFY_TOKEN
-    if SPOTIFY_TOKEN is None:
-        auth_response = await httpx.post(
-            "https://accounts.spotify.com/api/token",
-            data={
-                "grant_type": "client_credentials"
-            },
-            headers={
-                "Authorization": f"Basic {httpx.util.to_bytes(f'{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}')}"
-            }
-        )
-        if auth_response.status_code == 200:
-            SPOTIFY_TOKEN = auth_response.json().get("access_token")
+def get_spotify_token():
+    auth_string = f"{client_id}:{client_secret}"
+    auth_base64 = base64.b64encode(auth_string.encode()).decode()
+
+    headers = {
+        'Authorization' : f"Basic {auth_base64}",
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {'grant_type' : 'client_credentials'}
+    response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
+
+    if response.status_code == 200:
+        token_info = response.json()
+        access_token = token_info['access_token']
+        print(f"Access Token: {access_token}")
+        return access_token
+    else:
+        # print(f"Failed to get access token: {response,status_code}")
+        traceback.print_exc()
+        raise HTTPException(status_code=response.status_code, detail="Failed to authenticate")
+        print(response.json())
+        return None
+
+    
+def main():
+    try:
+        token = get_spotify_token()
+        if token:
+            print("Token retrieved")
         else:
-            raise HTTPException(status_code=500, detail="Failed to authenticate with Spotify API")
-    return SPOTIFY_TOKEN
+            print("Failed to receive token")
+    except Exception as e:
+        traceback.print_exc()
+        print(f"Exception: {e}")
+
+if __name__ == "__main__":
+    main()
